19d560bc991f31fe0b0838754b81cb5b17cec3f9
Mahamadou Sylla <mahamadousylla@Mahamadous-MacBook-Pro.local>
2018-02-03 16:25:56 -0800

    working on saving patient id to database

--- polls/models.py
+++ polls/models.py
@@ -4,3 +4,29 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Patient:
+	def __init__(self, code, access_token, refresh_token, expires_timestamp):
+		self.code = code
+		self.access_token = access_token
+		self.refresh_token = refresh_token
+		self.expires_timestamp = expires_timestamp
+
+	def get_code(self):
+		return self.code
+
+	def get_access_token(self):
+		return self.access_token
+
+	def get_refresh_token(self):
+		return self.refresh_token
+
+	def get_expires_timestamp(self):
+		return self.expires_timestamp
+
+	def set_code(self, code):
+		self.code = code
+
+
+class PatientID(models.Model):
+	app_label = "patient"
+	id = models.IntegerField(primary_key=True)
--- polls/views.py
+++ polls/views.py
@@ -4,35 +4,29 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.template.loader import get_template
 from django.http import HttpResponse
-from User import User
+from models import Patient, PatientID
 import requests, datetime, pytz
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-BASE_URL = "https://drchrono.com/"
-user = None
+URL_CHRONO = "https://drchrono.com/"
+URL_ONPATIENT = "https://onpatient.com/"
+CLIENT_ID = "YBWQ0HSOpwpumbLjqj83J6UMFqVsyqrG9uJ4Yrpz"
+CLIENT_SECRET = "RGocYqR3RWZXoc5C8FUf788boz3ZoBWpmudkoiHBCS4VR9c3LfoZ6BufBx5ZW7OkN889rVLhBy8jAIsuDfnTGYnquB7b9jH8CaKBsh5QvJo2OmmkugVFesrjBUyzWj9d"
+patient = None
 username = ""
 
 #helper methods
 def get_token(code):
-	'''takes in code and returns new user'''
+	'''takes in code and returns new patient'''
 	authorization = {
 		'code' : code,
     'grant_type': 'authorization_code',
     'redirect_uri': 'http://127.0.0.1:8000/home',
-    'client_id': 'YmHIOm9TT85ssvANTmxjX0mAqSfbCUX5mQ8tPumW',
-    'client_secret': 'T7dLCdvXXvGdvDHMqbIeS8jtuzvyeuvpx72WY4rUK3zs28tpnDahk4RPtQfdNBXK0Gn1fLXer93WFsUBg6TpcROLW7DHl74GqIVfAFlNa5m5myEuhZmhwsAFEVwOrT05',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
   }
 
-  # if user:
-  # 	code = user.get_code()
-
-  # authorization = {
-		# 'code' : code,
-  #   'grant_type': 'refresh_token',
-  #   'client_id': 'YmHIOm9TT85ssvANTmxjX0mAqSfbCUX5mQ8tPumW',
-  #   'client_secret': 'T7dLCdvXXvGdvDHMqbIeS8jtuzvyeuvpx72WY4rUK3zs28tpnDahk4RPtQfdNBXK0Gn1fLXer93WFsUBg6TpcROLW7DHl74GqIVfAFlNa5m5myEuhZmhwsAFEVwOrT05',
-  # }
-	response = requests.post(BASE_URL + "o/token/", data=authorization)
+	response = requests.post(URL_ONPATIENT + "o/token/", data=authorization)
 	response.raise_for_status()
 	data = response.json()
  
@@ -41,66 +35,94 @@ def get_token(code):
 	refresh_token = data['refresh_token']
 	expires_timestamp = datetime.datetime.now(pytz.utc) + datetime.timedelta(seconds=data['expires_in'])
 
-	user = User(code, access_token, refresh_token, expires_timestamp)
-	return user
+	patient = Patient(code, access_token, refresh_token, expires_timestamp)
+	return patient
 
-def get_username(user):
-	response = requests.get(BASE_URL + 'api/users/current', headers= {
-    'Authorization': 'Bearer %s' % user.get_access_token(),
+def refresh_token(patient):
+	'''refreshes patient token'''
+	response = requests.post(URL_ONPATIENT + 'o/token/', data={
+	  'refresh_token': patient.get_refresh_token(),
+	  'grant_type': 'refresh_token',
+	  'client_id': CLIENT_ID,
+	  'client_secret': CLIENT_SECRET,
+	})
+
+
+def get_patient_data(patient):
+	response = requests.get(URL_CHRONO + 'onpatient_api/fhir/Patient', headers= {
+  	'Authorization': 'Bearer %s' % patient.get_access_token(),
 	})
 
 	response.raise_for_status()
 	data = response.json()
 	return data
 
-def get_patient_data(user):
-	headers= {
-    'Authorization': 'Bearer ' + user.get_access_token(),
-	}
+def set_patient_data(data, patient):
+	data = data['results'][0]
+	for k, v in data.items():
+		print(k, v, "\n")
 
-	patients = []
-	patients_url = 'https://drchrono.com/api/patients'
-	while patients_url:
-		data = requests.get(patients_url, headers=headers).json()
-		patients.extend(data['results'])
-		patients_url = data['next'] # A JSON null on the last page
-	return patients
+	if not PatientID.objects.filter( id = data["identifier"] ).exists():
+		patient_obj = PatientID.objects.create( id = data["identifier"] )
+		patient_obj.save()
 
+	patient_data = []
+	patient_contact = []
+	name_dict = data["name"][0]
+	name =  " ".join(name_dict["family"]) + ", " + " ".join(name_dict["given"])
+	patient.name = " ".join(name_dict["given"])
+	# teleCom = data["telecom"]
+
+	patient_data.append( ("Name", name) )
+	patient_data.append( ("ID", data["identifier"]) )
+	patient_data.append( ("DOB", data["birthDate"]) )
+	patient_data.append( ("Gender", data["gender"]) )
+
+	for d in data["telecom"]:
+		print("d: ", d)
+		if d['system'] == 'email':
+			patient_data.append( ('Email', d['value']) )
+		elif d['use'] == 'home':
+			patient_data.append( ('Home Phone', d['value']) )
+		elif d['use'] == 'mobile':
+			patient_data.append( ('Cell Phone', d['value']) )
+	
+
+	for k, v in sorted(data["address"][0].items(), key = lambda p: p[0]):
+		if k == "line": patient_data.append( ("Street", " ".join(v)) )
+		elif k == "city": patient_data.append( ("City", v) )
+		elif k == "state": patient_data.append( ("State", v) )
+		elif k == "postalCode": patient_data.append( ("Zip Code", v) )
+
+	patient_data.append( ("Status", data["active"]) )
+
+	return patient_data
 
 #end of helper methods
+
 
 #start of views.
 def welcome(request):
 	return render(request, 'welcome.html')
 
 def home(request):
-	global user
+	global patient
 
-	if not user:
+	if not patient:
 		code = request.GET.get('code')
 		error = request.GET.get('error')
-		print(code, error, user)
+		print(code, error, patient)
 
 		if error: 
 			return render(request, 'error.html')
 
-		user = get_token(code)
-		data = get_username(user)
-		username = data['username']
-		user.username = username
-		patient_data = sorted(get_patient_data(user), key = lambda d: d["last_name"])
-		p_data = Paginator(patient_data, 10)
-		
-		# page = request.GET.get('page')
-		patient_data = p_data.page(1)
-		user.patient_data = patient_data
-		
-		for d in patient_data:
-			for k, v in d.items():
-				print(k, v)
-			print()
+		patient = get_token(code)
+		data = get_patient_data(patient)
+		patient.patient_data = set_patient_data(data, patient)
+		print("patient: ", patient.patient_data)
+			
 
-	return render(request, 'home.html', {'username': user.username, 'patients': user.patient_data})
+	return render(request, 'home.html', {'username': username, 'patients': patient.patient_data})
 
 def bp(request):
 	return render(request, 'bp.html', {'username': username})
@@ -115,6 +137,6 @@ def hydrate(request):
 	return render(request, 'hydrate.html', {'username': username})
 
 def logout(request):
-	global user
-	user = None
-	return render(request, 'index.html', {'username': username})
+	global patient
+	patient = None
+	return render(request, 'welcome.html')
